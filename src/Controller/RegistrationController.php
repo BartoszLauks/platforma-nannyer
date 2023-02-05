109a092d4a0c1026f35c728d13b53fd2655db052
BartoszLauks <bartosz.lauksstu@interia.pl>
2023-02-05 19:12:54 +0100

    Add Moduł pierwszego logowania, Akceptacja regulainu wspołpracy, Zmiana hasła.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,4 +46,10 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+
+    #[Route('/terms', name: 'app_terms')]
+    public function terms() : Response
+    {
+        return $this->render('registration/terms.html.twig');
+    }
 }
